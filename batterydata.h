506e9e00aae78883021f5364a1bca9ae2cef2375
Jukka Sarjanen <jukka-pekka.sarjanen@kone.com>
2015-06-02 21:15:52 +0300

    New class added

---
+++ batterydata.h
@@ -0,0 +1,18 @@
+#ifndef BATTERYDATA_H
+#define BATTERYDATA_H
+
+#include <QObject>
+
+class BatteryData : public QObject
+{
+    Q_OBJECT
+public:
+    explicit BatteryData(QObject *parent = 0);
+    ~BatteryData();
+
+signals:
+
+public slots:
+};
+
+#endif // BATTERYDATA_H
